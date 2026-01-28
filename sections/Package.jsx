24805f7bf0be157b8a9bd4e95361175258f57623
Nilesh Akmeemana <nileshnirmalakmeemana@gmail.com>
2026-01-28 12:33:51 +0530

    removed pricing column

--- sections/Package.jsx
+++ sections/Package.jsx
@@ -199,7 +199,7 @@ export default function Example() {
                 <h1 className="text-3xl font-semibold text-center mx-auto">Package Features</h1>
                 <p className="text-sm text-slate-500 text-center mt-2 max-w-md mx-auto">Everything you need for a beautifully styled celebration custom decor, seamless setup, and stress free service.</p>
                 
-                <div className="border border-gray-200 rounded-lg max-w-5xl mx-auto mt-16 grid grid-cols-1 divide-y divide-gray-200 lg:grid-cols-3 lg:divide-x lg:divide-y-0">
+                <div className="border border-gray-200 rounded-lg max-w-5xl mx-auto mt-16 grid grid-cols-1 divide-y divide-gray-200 lg:grid-cols-2 lg:divide-x lg:divide-y-0">
                     <div className="p-8 flex flex-col hover:bg-gray-50 transition duration-300">
                         <h1 className="text-3xl font-semibold text-slate-700">{packageData.Package_Name}</h1>
                         {/* <h3 className="text-base font-semibold text-slate-700">{packageData.Package_Name}</h3> */}
